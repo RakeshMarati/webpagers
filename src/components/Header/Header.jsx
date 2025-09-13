11f909687303c0a62d3450099575a13d8c052f6a
rakesh <rakesh.marati01@gmail.com>
2025-09-13 23:41:00 +0530

    Header Added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1>My E-Commerce Store</h1>
+        <p>Welcome to our online shopping store</p>
+    </div>
+  )
+}
+
+export default Header
